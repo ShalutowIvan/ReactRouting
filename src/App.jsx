d5ef1c37ffbb303fb9bd03cdf7fb861d5fa3eda7
Ivan Shalutov <ivan_shalutow@mail.ru>
2025-01-17 21:25:48 +0700

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -17,40 +17,40 @@ import Editpost from './pages/Editpost';
 
 import Layout from './components/Layout'
 
-
-
+import { RouterProvider } from "react-router-dom"
+import Router from './Router'
 
 function App() {
   
 
   return (
     
+      <RouterProvider router={ Router }></RouterProvider>
 
-      <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Homepage />} />        
-          <Route path="posts" element={<Blogpage />} />          
-          <Route path="posts/:id" element={<Singlepage />} />
-          
-          <Route path="posts/:id/edit" element={<Editpost />} />
-          <Route path="about" element={<Aboutpage />} />
-          <Route path="about-us" element={<Navigate to="/about" replace />} />
-          <Route path="*" element={<Notfoundpage />} />
-          
-          {/*<Route path="posts/new" element={<Createpost />} />*/}
-          <Route path="posts/new" element={
-            <Createpost />
-          } action={createPostAction} />
-
-
-        </Route>
-      </Routes>
-
-      </>
-
-    
+        
   )
 }
 
 export default App
+
+
+
+      // <Routes>
+      //   <Route path="/" element={<Layout />}>
+      //     <Route index element={<Homepage />} />        
+      //     <Route path="posts" element={<Blogpage />} />          
+      //     <Route path="posts/:id" element={<Singlepage />} />
+          
+      //     <Route path="posts/:id/edit" element={<Editpost />} />
+      //     <Route path="about" element={<Aboutpage />} />
+      //     <Route path="about-us" element={<Navigate to="/about" replace />} />
+      //     <Route path="*" element={<Notfoundpage />} />
+          
+      //     {/*<Route path="posts/new" element={<Createpost />} />*/}
+      //     <Route path="posts/new" element={
+      //       <Createpost />
+      //     } action={createPostAction} />
+
+
+      //   </Route>
+      // </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,9 @@ import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <BrowserRouter>
+  
     <App />
-    </BrowserRouter>
+  
     </StrictMode>
 )
 
